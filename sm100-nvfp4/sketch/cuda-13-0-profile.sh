ebfcef7018911756c94438254e6982327ad6bd48
root <root@steel-kingfisher-of-recreation.datacrunch.io>
2026-07-02 15:45:26 +0000

    updated

---
+++ sm100-nvfp4/sketch/cuda-13-0-profile.sh
@@ -0,0 +1,4 @@
+export CUDA_HOME=/usr/local/cuda
+export CUDA_PATH=/usr/local/cuda
+export PATH=/usr/local/cuda/bin:${PATH}
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
